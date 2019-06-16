2de63da0d2de25e856085c21ae447b2bf5a7bf07
hiko_t <yotti4415@gmail.com>
2019-06-16 09:13:06 +0000

    デバッグモードとルーティングのコードを追加

--- hello.py
+++ hello.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 
 # routeデコーダでURLを指定する
 @app.route('/')
+def index():
+    return 'Index Page'
+
+# ルーティングで切り替え
+@app.route('/hello')
 def hello_world():
+# 始まりの関数
     return "Hello World!"
 
 # Pythonインタプリタから直接実行されたときに
@@ -15,6 +21,9 @@ def hello_world():
 if __name__ == '__main__':
     # 外部からアクセスを許可するための設定
     app.run(host='0.0.0.0')
+
+    # デバッグモードを指定
+    app.debug = True
 
     # アプリケーションの実行
     app.run()
